ff239f4d8cbbc078bfa5042b1721f4bb8ef6822d
lovelazur01 <jkuj66123!>
2021-03-15 00:37:55 +0900

    무중단 배포 스크립트 작성 001

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -2,34 +2,36 @@
 
 #쉬고 있는 profile 찾기 , real 1이 사용중이면 real2가 쉬고 있음
 
-function find_idle_profile() {
-  RESPONSE_CODE = $(curl -s -o /dev/null -w "%http_code") http://locahost/profile
+function find_idle_profile()
+{
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [ "$RESPONSE_CODE" -ge 400 ]; # greater than 400 code
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
-      CURRENT_PROFILE=real2
+        CURRENT_PROFILE=real2
     else
-      CURRENT_PROFILE=$(curl -s http://localhost/profile)
-  fi
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
 
-  if [ "${CURRENT_PROFILE}" == real1 ]
+    if [ ${CURRENT_PROFILE} == real1 ]
     then
       IDLE_PROFILE=real2
     else
       IDLE_PROFILE=real1
-  fi
+    fi
 
-  echo ${IDLE_PROFILE}
-
+    echo "${IDLE_PROFILE}"
 }
 
-#쉬고 있는 profile 포트 찾
-function find_idle_port() {
-  IDLE_PROFILE=$(find_idle_profile)
-  if[ "${IDLE_PROFILE}" == real1 ]
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port()
+{
+    IDLE_PROFILE=$(find_idle_profile)
+
+    if [ ${IDLE_PROFILE} == real1 ]
     then
       echo "8081"
     else
       echo "8082"
-  fi
+    fi
 }
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname "$ABSPATH")
-source "${ABSDIR}"/profile.sh
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot2-webservices
+PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
@@ -27,6 +27,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-"$IDLE_PROFILE".properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-    -Dspring.profiles.active="$IDLE_PROFILE" \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
